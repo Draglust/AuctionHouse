e5639b2e5d3cf18fe45b077bad4bdeb4f36fafa6
Carlos <carlosluquegutierrez@gmail.com>
2021-02-07 16:41:50 +0100

    Actualizar pin-blue.png, pin-green.png y maps.blade.php

--- resources/views/maps.blade.php
+++ resources/views/maps.blade.php
@@ -17,19 +17,19 @@
                         @foreach($maps as $key => $map)
                         <div class="row">
                             <div class="col-md-12">
-                                    <div style="max-width: 1002px; max-height: 668px; cursor: pointer; display:block;position:relative">
+                                    <div style="max-width: 1002px; max-height: 668px; display:block;position:relative">
                                         <img style="max-width: 1002px; max-height: 668px;display:block;margin-left:50%;" src="https://wow.zamimg.com/images/wow/maps/eses/original/{{$key}}.jpg" alt="">
                                         @if(isset($map['values']['coords_normal']))
                                             @foreach($map['values']['coords_normal'] as $coord)
                                                 <div style="left:{{$coord[0]}}%;top:{{$coord[1]}}%;position:absolute;width:1px;height:1px;font-size:1px;z-index:5;margin-left:50%;">
-                                                    <img src="{{asset('resources/images/pin-yellow.png')}}" style="position:relative;width:11px;height:11px;display:block">
+                                                    <img src="{{asset('resources/images/pin-green.png')}}" style="position:relative;width:11px;height:11px;display:block">
                                                 </div>
                                             @endforeach
                                         @endif
                                         @if(isset($map['values']['coords_elite']))
                                             @foreach($map['values']['coords_elite'] as $coord)
                                                 <div style="left:{{$coord[0]}}%;top:{{$coord[1]}}%;position:absolute;width:1px;height:1px;font-size:1px;z-index:5;margin-left:50%;">
-                                                    <img src="{{asset('resources/images/pin-blue.png')}}" style="position:relative;width:11px;height:11px;display:block">
+                                                    <img src="{{asset('resources/images/pin-yellow.png')}}" style="position:relative;width:11px;height:11px;display:block">
                                                 </div>
                                             @endforeach
                                         @endif
